83dfeb3a4baedcb481f2be25f71f46f55a3ab27e
Christopher Olm <caugolm@gmail.com>
2026-05-27 16:36:22 -0400

    fix output directory for flash dots. start dot summarizer

--- bin/submit_dots_flash_sumarries.sh
+++ bin/submit_dots_flash_sumarries.sh
@@ -3,19 +3,19 @@
 bidsBase=/project/ftdc_pipeline/pmc_exvivo/oriented/bids/
 purplerepo=/project/ftdc_pipeline/purple_code/purple-mri_20250919/
 scriptsdir=/project/ftdc_pipeline/ftdc-picsl/pmacsPurps-v0.2.1/bin/
-outdir=${bidsBase}/derivatives/beta_FLASH_T2w/
 
 if [[ $# -lt 1 ]] ; then
-    echo "./submit_dots_to_flashreorient.sh <filelist,<<optional:hemi>>.csv> "
-    echo "  wrapper for mapping the dots placed on t2w-reslice to the FLASH-reorient"
+    echo "./submit_dots_flash_summaries.sh <filelist,<<optional:hemi>>.csv> "
+    echo "  wrapper for using c3d to summarize the dots to FLASH-reorient instensities"
     echo "  filelist,hemi.csv should be path to a reoriented bids nifti file in /anat/ , relative to $bidsin, hemi is ignored (but allowed for to use same csv as for other stuff)"
-    echo "  output goes to $outdir "
+    echo "  output goes to $outBase "
     exit 1
 fi
 
 filelist=$1
 n_threads=1
 
+outdir=${bidsBase}/derivatives/
 logdir=${outdir}logs/
 if [[ ! -d $logdir ]] ; then 
     mkdir -p $logdir
@@ -53,30 +53,6 @@ for x in `cat $filelist `; do
         # continue skips rest of loop for this line if no anat dir
         continue
     fi 
-    echo "${reslice} found"
-    echo "" 
-
-    # check for the t2w-reslice to t2w-reorient mat 
-    reslice_mat_i=$(echo $reslice_i | sed 's/\.nii\.gz/\.mat/')
-    reslice_mat=`ls ${bidsBase}/${reslice_mat_i} 2> /dev/null`
-    if [[ ! -f $reslice_mat ]] ; then
-        echo "no file named ${bidsBase}/${reslice_mat_i} ...skipping"
-        # continue skips rest of loop for this line if no anat dir
-        continue
-    fi 
-    echo "${reslice_mat} found"
-    echo "" 
-
-    # check for the t2w-reslice dots image
-    reslice_dots_i=$(echo $reslice_i | sed "s/acq-300um_rec-${reslice_bidschunk}_T2w/rec-reslice_space-T2w_cortexdots_final/")
-    reslice_dots=`ls ${bidsBase}/${reslice_dots_i} 2> /dev/null`
-    if [[ ! -f $reslice_dots ]] ; then
-        echo "no file named ${bidsBase}/${reslice_dots_i} ...skipping"
-        # continue skips rest of loop for this line if no anat dir
-        continue
-    fi 
-    echo "${reslice_dots} found"
-    echo "" 
 
     # check for the flash-reorient image
     flash_i=$(echo $reslice_i | sed "s/acq-300um_rec-${reslice_bidschunk}_T2w/acq-160um_echo-2_run-3_rec-reorient_FLASH/")
@@ -92,19 +68,19 @@ for x in `cat $filelist `; do
     # check for the flash-reorient dots image
     flash_dots_i=$(echo $reslice_i | sed "s/acq-300um_rec-${reslice_bidschunk}_T2w/rec-reorient_space-FLASH_cortexdots_final/")
     flash_dots=`ls ${outdir}/${flash_dots_i} 2> /dev/null`
-    if [[ -f $flash_dots ]] ; then
-        echo "file ${flash_dots} exists ...skipping"
+    if [[ ! -f $flash_dots ]] ; then
+        echo "no file ${outdir}/${flash_dots} exists ...skipping"
         # continue skips rest of loop for this line if no anat dir
         continue
     fi 
-    echo "${outdir}/${flash_dots_i} not found ... proceeding"
+    
     echo "" 
 
 
-    cmd="${scriptsdir}/reslice_dots_to_flashreorient.sh ${reorient} ${reslice} ${reslice_mat} ${reslice_dots} ${flash} ${outdir}/${flash_dots_i} ${scriptsdir}"
+    cmd="${scriptsdir}/dots_flash_summary.sh  ${reslice_dots} ${flash} ${outdir}/${flash_dots_i} ${scriptsdir}"
     echo $cmd 
     # $cmd
-    bsub -N -J ${filestem}_reslice_dots_to_flashreorient -o ${logdir}/${filestem}_reslice_dots_to_flashreorient_%J.txt -n 4 -M 64GB $cmd
+    # bsub -N -J ${filestem}_reslice_dots_to_flashreorient -o ${logdir}/${filestem}_reslice_dots_to_flashreorient_%J.txt -n 4 -M 12GB $cmd
 
 done
 
